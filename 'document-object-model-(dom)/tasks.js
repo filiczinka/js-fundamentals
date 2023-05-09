b509ff1e88ae605d402d21f4b15d8cd9264d3a18
Filiczinka <filiczinka@gmail.com>
2023-05-09 22:11:41 +0300

    add dom hw

--- 'document-object-model-(dom)/tasks.js
+++ 'document-object-model-(dom)/tasks.js
@@ -14,6 +14,18 @@
 Результат виводу: 1, 5, 2, 4, 3*/
 
 
+const list = document.getElementById('list');
+
+const items = [
+	list.firstElementChild.textContent, // перший елемент 
+	list.lastElementChild.textContent, // останній елемент 
+	list.children[1].textContent, // другий елемент 
+	list.children[3].textContent, // четвертий елемент 
+	list.children[2].textContent // третій елемент 
+];
+alert(items.join(', '));
+
+
 
 
 /*2. 
@@ -35,7 +47,26 @@
 </body>
 Напишіть скріпт, який за допомогою засобів DOM простилізує сторінку так як показано на картинці.*/
 
+const h1 = document.querySelector("h1");
+h1.style.color = "black";
+h1.style.backgroundColor = "#93c47d";
 
+const myDiv = document.getElementById('myDiv');
+const paragraphs = [
+	myDiv.firstElementChild.style.fontWeight = 'bold', // перший елемент 
+	myDiv.children[1].style.color = 'red', // другий елемент 
+	myDiv.children[2].style.textDecoration = 'underline', // третій елемент 
+	myDiv.children[3].style.fontStyle = 'italic', // четвертий елемент 
+];
+
+const listItems = document.querySelectorAll('#myList li');
+
+listItems.forEach(function (item) {
+	item.style.display = 'inline-block';
+});
+
+const span = document.querySelector('span');
+span.style.display = 'none';
 
 
 /*3. 
@@ -48,20 +79,57 @@
  </main> 
 </body>*/
 
+const body = document.body;
+const main = document.createElement('main');
+main.setAttribute('class', 'mainClass check item');
 
+const div = document.createElement('div');
+div.setAttribute('id', 'myDiv');
+
+const p = document.createElement('p');
+p.textContent = 'First paragraph';
+
+div.appendChild(p);
+main.appendChild(div);
+body.appendChild(main);
 
 
 /*4.
 Задача описана в блоці JS
 https://codepen.io/misha_klymenko/pen/Jjabvez*/
 
+const btn = document.querySelector('.btn');
+const outBlock = document.querySelector('.out');
 
+btn.addEventListener('click', function() {
+	const inputs = document.querySelectorAll('.arr');
+	const data = [];
+	
+	inputs.forEach(function(input) {
+		data.push(input.value);
+	});
+	
+	outBlock.innerHTML = data.join('<br>');
+});
 
 
 /*5. 
 Задача описана в блоці JS
 https://codepen.io/misha_klymenko/pen/abzLvqo*/
+const circles = document.querySelectorAll('.circle');
 
+circles.forEach(circle => {
+  const animValue = circle.dataset.anim;
+  circle.classList.add(animValue);
+});
+
+circles.forEach(circle => {
+  const classList = circle.classList;
+  const hasAnimation = Array.from(classList).some(className => className.startsWith('animate-'));
+  const message = hasAnimation ? 'Анімація була застосована.' : 'Анімація не була застосована.';
+
+  console.log(`Для елемента з класом 'circle' ${message}`);
+});
 
 
 
@@ -69,6 +137,32 @@ https://codepen.io/misha_klymenko/pen/abzLvqo*/
 Задача описана в блоці JS
 https://codepen.io/misha_klymenko/pen/dyjyeGO */
 
+const colors = document.querySelectorAll('.color');
+const sizes = document.querySelectorAll('.size');
+
+const price = document.getElementById('outprice');
+
+let currentPrice = colors[0].dataset.price;
+
+colors.forEach(color => {
+  color.addEventListener('click', () => {
+    document.querySelector('.color.active').classList.remove('active');
+    color.classList.add('active');
+
+    currentPrice = color.dataset.price;
+    price.innerText = currentPrice;
+  });
+});
+
+sizes.forEach(size => {
+  size.addEventListener('click', () => {
+    document.querySelector('.size.active').classList.remove('active');
+    size.classList.add('active');
+    
+    currentPrice = size.dataset.price;
+    price.innerText = currentPrice;
+  });
+});
 
 
 
